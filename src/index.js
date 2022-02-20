9dfeb1264bbfab00294fe6d4247da18dbc00236e
thiyagarajan mohan <thiyaguj19@live.com>
2022-02-20 14:22:38 -0800

    passing function and attribute as props to each function component

--- src/index.js
+++ src/index.js
@@ -1,19 +1,39 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-function Button() {
-  const [counter, setCounter] = useState(5);
+function Button(props) {
+
+
+  //const handleClick = () => setCounter(counter*2);
   //use regular function
   //return <button onClick={logRandom}>{counter}</button>
   //using arrow function
   //return <button onClick={() => alert(Math.random())}> {counter} </button>;
   //return <button onClick={() => setCounter(counter+1)}> {counter} </button>;
-  return <button onClick={() => setCounter(counter*2)}> {counter} </button>;
+  return <button onClick={props.handlClick}> Multiply by 2 </button>;
+}
+
+function Display(props) {
+  return (
+    <div>{props.counter}</div>
+  )
+}
+
+function App() {
+
+  const [counter, setCounter] = useState(5);
+  const handleClick = () => setCounter(counter*2);
+  return (
+    <div>
+      <Button handlClick={handleClick}/>
+      <Display counter={counter}/>
+    </div>
+  )
 }
 
 ReactDOM.render(
   <React.StrictMode>
-    <Button />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
