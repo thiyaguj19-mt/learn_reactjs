cc9f3e37cf48c90924892d54508aa5672029f7ea
thiyagarajan mohan <thiyaguj19@live.com>
2022-02-20 14:09:32 -0800

    first function component in reactjs

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+function Button() {
+  const [counter, setCounter] = useState(5);
+  //use regular function
+  //return <button onClick={logRandom}>{counter}</button>
+  //using arrow function
+  //return <button onClick={() => alert(Math.random())}> {counter} </button>;
+  //return <button onClick={() => setCounter(counter+1)}> {counter} </button>;
+  return <button onClick={() => setCounter(counter*2)}> {counter} </button>;
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Button />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
